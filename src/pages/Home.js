9be8633a2af5263bf5b2b8fa01d2622eda40720e
te70 <tevinkinuthia@gmail.com>
2024-01-08 18:38:01 +0300

    Add homepage files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,30 @@ import WomanImg from '../img/home/woman.png';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <section className='section'>
-    <div className='container mx-auto'>
+  return (<section className='section'>
+    <div className='container mx-auto h-full relative'>
       {/* text & img wrapper */}
+      <div className='flex flex-col justify-center'>
+        {/* text */}
+        <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>
+            photographer <br /> & film maker
+          </h1>
+          <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
+            Los Angeles, USA
+          </p>
+          <Link to={'/contact'} className='btn mb-[30px]'>hire me</Link>
+        </div>
+         {/* image */}
+      <div className='flex justify-end max-h-96 lg:max-h-max'>
+        <div className='relative lg:-right-40 overflow-hidden'>
+          <img src={WomanImg} alt='' />
+        </div>
+      </div>
+      </div>
     </div>
-  </section>;
+  </section>
+  );
 };
 
 export default Home;
