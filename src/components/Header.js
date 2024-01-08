34967bcd995379bdca9cdf93f3bf0b0106d7cbb2
te70 <tevinkinuthia@gmail.com>
2024-01-08 17:48:59 +0300

    Add nav & mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,14 +12,21 @@ const Header = () => {
   h-[100px] lg:h-[140px] flex items-center'>
     <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
       {/* logo */}
-      <Link to={'/'}>
+      <Link to={'/'} className='max-w-[200px]'>
         <img src={Logo} alt=''/>
       </Link>
       {/* nav - initially hidden - show on desktop mode */}
-      <nav className='hidden lg:flex'>
+      <nav className='hidden lg:flex gap-x-12 font-semibold'>
         <Link to={'/'} className="text-[#696c6d] hover:text-primary transition">Home</Link>
+        <Link to={'/about'} className="text-[#696c6d] hover:text-primary transition">About</Link>
+        <Link to={'/portfolio'} className="text-[#696c6d] hover:text-primary transition">Portfolio</Link>
+        <Link to={'/contact'} className="text-[#696c6d] hover:text-primary transition">Contact</Link>
       </nav>
     </div>
+    {/* Socials */}
+    <Socials />
+      {/* mobile nav */}
+      <MobileNav />
   </header>;
 };
 
