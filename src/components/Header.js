9c33067581a814b42119e0ec1d5549229f84cd30
te70 <tevinkinuthia@gmail.com>
2024-01-08 17:02:35 +0300

    App structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+//import components
+import Socials from './Socials';
+import Logo from '../img/header/logo.svg';
+import MobileNav from './MobileNav';
+
+//import link
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 
+  h-[100px] lg:h-[140px] flex items-center'>
+    <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+      {/* logo */}
+      <Link to={'/'}>
+        <img src={Logo} alt=''/>
+      </Link>
+      {/* nav - initially hidden - show on desktop mode */}
+      <nav className='hidden lg:flex'>
+        <Link to={'/'} className="text-[#696c6d] hover:text-primary transition">Home</Link>
+      </nav>
+    </div>
+  </header>;
 };
 
 export default Header;
