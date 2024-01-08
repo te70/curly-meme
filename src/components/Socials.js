0dbfcf71ae3f9ee4beadcc2fac2d0c9d70dc98e3
te70 <tevinkinuthia@gmail.com>
2024-01-08 18:04:35 +0300

    Add socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+// import icons
+import { 
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube
+} from 'react-icons/im';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='https://www.facebook.com' target='_blank'>
+          <ImFacebook/>
+        </a>
+      </li>
+      <li>
+        <a href='https://www.twitter.com' target='_blank'>
+          <ImTwitter/>
+        </a>
+      </li>
+      <li>
+        <a href='https://www.pintrest.com' target='_blank'>
+          <ImPinterest/>
+        </a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com' target='_blank'>
+          <ImInstagram/>
+        </a>
+      </li>
+      <li>
+        <a href='https://www.youtube.com' target='_blank'>
+          <ImYoutube/>
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+//import images
+import WomanImg from '../img/home/woman.png';
+//import link
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <section className='section bg-blue-200'>Home</section>;
+  return <section className='section'>
+    <div className='container mx-auto'>
+      {/* text & img wrapper */}
+    </div>
+  </section>;
 };
 
 export default Home;
